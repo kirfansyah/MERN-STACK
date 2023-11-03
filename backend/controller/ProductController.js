721f964862270f75eaa6359900821d8a980f9981
Kiki Irfansyah <kikiirfansyah33@gmail.com>
2023-11-03 09:28:13 +0700

    Backend Done..

---
+++ backend/controller/ProductController.js
@@ -0,0 +1,68 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+export const getProductById = async (req, res) => {
+  try {
+    const products = await prisma.product.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(404).json({ msg: error.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const { name, price } = req.body;
+  try {
+    const product = await prisma.product.create({
+      data: {
+        name,
+        price,
+      },
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const updateProduct = async (req, res) => {
+  const { name, price } = req.body;
+  try {
+    const product = await prisma.product.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name,
+        price,
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const deleteProduct = async (req, res) => {
+  const { name, price } = req.body;
+  try {
+    const product = await prisma.product.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
